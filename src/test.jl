9d7ba03207f4c78c626b0c5520046996f97f337f
Peter Rohde <dr.rohde@gmail.com>
2021-01-27 14:31:06 +1100

    working multi-user demo
    working multi-user demo

--- src/test.jl
+++ src/test.jl
@@ -3,7 +3,7 @@ include("QuNet.jl")
 using LightGraphs, SimpleWeightedGraphs, GraphPlot, MetaGraphs, Plots, Colors
 
 net = QuNet.GridNetwork(10,10)
-temp = QuNet.TemporalGraph(net,3)
+temp = QuNet.TemporalGraph(net,1)
 
 g = deepcopy(temp.graph["loss"])
 
@@ -12,3 +12,6 @@ QuNet.plot_network(g, user_paths, temp.locs_x, temp.locs_y)
 
 #gplot(g, edgestrokec=colors, edgelinewidth=widths, arrowlengthfrac=0.04, layout=spring_layout, nodelabel=1:nv(g))
 #gplot(g, arrowlengthfrac=0.04)
+
+# QuNet.refresh_graph(net)
+# QuNet.percolation_bench(net.graph, 0.1, 100)
